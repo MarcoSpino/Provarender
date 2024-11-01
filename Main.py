b4988709d5e8065c1d85b52f412d4cfca53f1e06
MarcoSpino <51476523+MarcoSpino@users.noreply.github.com>
2024-11-01 12:54:56 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ from pyppeteer import launch
 
 async def main():
 	browser = await launch()
- 	page = await browser.newPage()
- 	await page.goto('https://example.com')
- 	await page.screenshot({'path': 'example.png'})
- 	await browser.close()
+	page = await browser.newPage()
+	await page.goto('https://example.com')
+	await page.screenshot({'path': 'example.png'})
+	await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
