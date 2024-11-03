3afa9195ea6fa71c54c963af41c48e3e2dcd94b3
MarcoSpino <marco01spinelli@gmail.com>
2024-11-03 01:25:20 +0100

    Render Login V3

--- Main.py
+++ Main.py
@@ -18,16 +18,42 @@ import asyncio
 from pyppeteer import launch
 
 class BrowserHandler:
+        
     def __init__(self):
         self.browser = None
         self.page = None
+        
+        self.hrefs = []
+        self.missionList = []
+        self.vehicleList = []
+        self.despatches = []
+        self.missionsSeen = []
+        self.buildingList = []
+        init()
+        logged_in = self.Login()
+        if logged_in:
+            self.edificiList()
+            self.veicoliList()
+            self.personale()
+            while True:
+                try:
+                    self.buildMissionList()
+                    self.doMision()
+                    print(Fore.LIGHTBLUE_EX, "Aspetto 5 secondi")
+                    time.sleep(5)
+                except Exception as e:
+                    print(Fore.RED + "Oh no, an error occurred." + Style.RESET_ALL)
+                    print(e)
+                    print(Fore.RED + "Restarting bot...." + Style.RESET_ALL)
+        else:
+            print("Couldn't log in...")
 
     async def initialize(self):
         # Inizializza il browser e la pagina
         self.browser = await launch(headless=True)
         self.page = await self.browser.newPage()
-
-    async def fetch_content(self):
+        
+    async def Login(self):
         print("Sono qui2")
         self.page.goto('https://www.operatore112.it/users/sign_in')
         self.page.waitForSelector('user_email')
@@ -37,13 +63,9 @@ class BrowserHandler:
         self.page.click('#submit-button-id')
         print("Sono qui")
         try:
-            # check we are logged in- by grabbing a random tag only visible on log in.
             alliance = self.page.querySelector('#alliance_li')
             if alliance:
-                # Ottieni il valore dell'attributo 'class'
                 class_name = (alliance.getProperty('className')).jsonValue()
-            
-                # Controlla se la classe è uguale a "dropdown"
                 if class_name == "dropdown":
                     print("Logged in")
                     return True
@@ -51,10 +73,6 @@ class BrowserHandler:
                 return False
         except Exception as e:
             return False
-
-    async def close(self):
-        # Chiude il browser
-        await self.browser.close()
 
 async def main():
     # Crea un'istanza della classe
@@ -67,6 +85,8 @@ async def main():
     
     # Chiudi il browser
     await handler.close()
+
+
 
 # Esegui il codice asincrono
 asyncio.get_event_loop().run_until_complete(main())
