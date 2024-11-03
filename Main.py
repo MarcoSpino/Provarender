3803095cdfc3b7a3acb75ece4708052f52737c29
MarcoSpino <marco01spinelli@gmail.com>
2024-11-03 11:45:00 +0100

    Render Login V5

--- Main.py
+++ Main.py
@@ -18,6 +18,27 @@ import asyncio
 from pyppeteer import launch
 
 class BrowserHandler:
+
+    async def Login(self):
+            print("Sono qui2")
+            self.page.goto('https://www.operatore112.it/users/sign_in')
+            self.page.waitForSelector('user_email')
+            self.page.type('user_email', 'Marco01spino@gmail.com')
+            self.page.waitForSelector('user_password')
+            self.page.type('user_password', 'Gemelli@2001')
+            self.page.click('#submit-button-id')
+            print("Sono qui")
+            try:
+                alliance = self.page.querySelector('#alliance_li')
+                if alliance:
+                    class_name = (alliance.getProperty('className')).jsonValue()
+                    if class_name == "dropdown":
+                        print("Logged in")
+                        return True
+                else:
+                    return False
+            except Exception as e:
+                return False
         
     def __init__(self):
         self.browser = None
@@ -47,27 +68,7 @@ class BrowserHandler:
         # Inizializza il browser e la pagina
         self.browser = await launch(headless=True)
         self.page = await self.browser.newPage()
-        
-    async def Login(self):
-        print("Sono qui2")
-        self.page.goto('https://www.operatore112.it/users/sign_in')
-        self.page.waitForSelector('user_email')
-        self.page.type('user_email', 'Marco01spino@gmail.com')
-        self.page.waitForSelector('user_password')
-        self.page.type('user_password', 'Gemelli@2001')
-        self.page.click('#submit-button-id')
-        print("Sono qui")
-        try:
-            alliance = self.page.querySelector('#alliance_li')
-            if alliance:
-                class_name = (alliance.getProperty('className')).jsonValue()
-                if class_name == "dropdown":
-                    print("Logged in")
-                    return True
-            else:
-                return False
-        except Exception as e:
-            return False
+    
 
 async def main():
     # Crea un'istanza della classe
