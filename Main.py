eace1fad7e730aad710d368051aafe8a6822202b
MarcoSpino <marco01spinelli@gmail.com>
2024-11-03 22:05:23 +0100

    Render Playwright Prova

--- Main.py
+++ Main.py
@@ -1,29 +1,31 @@
-import asyncio
-from pyppeteer import launch
 
 import subprocess
 import sys
 def install():
     subprocess.check_call([sys.executable, "-m", "playwright", "install"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "colorama"])
 
 install()
 
-from colorama import init, Fore, Style
-import time
-import sys
-import os
-
-
-# Install Pyppeteer
-import subprocess
-import sys
-subprocess.check_call([sys.executable, "-m", "playwright", "install"])
-
 from playwright.sync_api import sync_playwright
 
+# Avvia Playwright
 with sync_playwright() as p:
+    # Lancia il browser (in modalità headless)
     browser = p.chromium.launch(headless=True)
+    
+    # Apre una nuova pagina
     page = browser.new_page()
+    
+    # Visita example.com
     page.goto("https://example.com")
-    page.screenshot(path="example.png")
+    
+    # Prende il titolo della pagina
+    titolo = page.title()
+    
+    # Stampa il titolo della pagina
+    print("Titolo della pagina:", titolo)
+    
+    # Chiude il browser
     browser.close()
+
